4fcbdedc7bc9193130b5d934b4a5e4b08f9f23f5
CristianRojas <smumo0377@gmail.com>
2024-05-28 19:02:17 +0200

    condicional experimental

---
+++ src/Condicional experimental.kt
@@ -0,0 +1,36 @@
+fun main() {
+    //problema 1
+    print("ingrese primer valor")
+    val valor1 = readln().toInt()
+    print("ingrese segundo valor")
+    val valor2 = readln().toInt()
+    val mayor = if (valor1 > valor2) valor1 else valor2
+    println("el mayor entre $valor1 y $valor2 es $mayor")
+}
+
+
+
+fun main(){
+    print("ingrese un valor entero:")
+    val valor = readln().toInt()
+    val resultado = if (valor % 2 == 0){
+        print("Cuadrado:")
+        valor * valor
+    }else {
+        print("Cubo:")
+        valor * valor * valor
+    }
+    print(resultado)
+
+
+
+    //problema propuesto
+    print("ingrese un numero entre el 1 y el 99:")
+    val numero = readln().toInt()
+    val cantidad = if (numero >= 10){
+        println("el numero tiene dos digitos")
+    }else {
+        println("el numero tiene un digito")
+}
+    }
+
