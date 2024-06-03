79f7ee04951764d42db20b293d41e010f110bd8e
CristianRojas <smumo0377@gmail.com>
2024-06-03 21:54:41 +0200

    Estructuras_condicionales_anidadas

--- src/Condicional experimental.kt
+++ src/Condicional experimental.kt
@@ -1,15 +1,19 @@
+
 fun main() {
     //problema 1
+    /*
     print("ingrese primer valor")
     val valor1 = readln().toInt()
     print("ingrese segundo valor")
     val valor2 = readln().toInt()
     val mayor = if (valor1 > valor2) valor1 else valor2
     println("el mayor entre $valor1 y $valor2 es $mayor")
+
+     */
 }
 
 
-
+/*
 fun main(){
     print("ingrese un valor entero:")
     val valor = readln().toInt()
@@ -21,9 +25,9 @@ fun main(){
         valor * valor * valor
     }
     print(resultado)
+*/
 
-
-
+/*
     //problema propuesto
     print("ingrese un numero entre el 1 y el 99:")
     val numero = readln().toInt()
@@ -32,5 +36,8 @@ fun main(){
     }else {
         println("el numero tiene un digito")
 }
-    }
+
+ */
+
+
 
---
+++ src/Estructuras _condicionales_anidadas.kt
@@ -0,0 +1,58 @@
+fun main() {
+    //problema 1
+    /*
+    print("ingrese primer nota:")
+    val nota1: Int = readln().toInt()
+    print("ingrese segunda nota:")
+    val nota2: Int = readln().toInt()
+    print("ingrese tercer nota")
+val nota3: Int = readln().toInt()
+
+    val promedio = (nota1 + nota2 + nota3) / 3
+    if (promedio >= 7)
+        print("Promociones")
+    else
+        if (promedio >= 4)
+            print("Regular")
+    else
+        print("Reprobado")
+
+
+ */
+    /*
+    print("ingrese primer nota:")
+    val nota1: Int = readln().toInt()
+    print("ingrese segunda nota:")
+    val nota2: Int = readln().toInt()
+    print("ingrese tercer nota")
+    val nota3: Int = readln().toInt()
+
+    val promedio = (nota1 + nota2 + nota3) / 3
+
+    val estado = if (promedio >= 7) "promocionado"
+    else if (promedio >= 4) "regular" else "reprobado"
+    print("Estado del alumno $estado")
+
+
+     */
+/*
+    print("Ingrese el primer número")
+    val num1: Int = readln().toInt()
+    print("Ingrese el segundo número")
+    val num2: Int = readln().toInt()
+    print("Ingrese el tercer número")
+    val num3: Int = readln().toInt()
+    if (num1 > num2)
+        if (num1 > num3)
+            print("el numero mayor es: $num1")
+    else
+        print("El numero mayor es: $num3")
+    else if (num2 > num3)
+        print("El numero mayor es: $num2")
+    else if (num3 > num2)
+        print("El numero mayor es: $num3")
+
+ */
+
+
+}
